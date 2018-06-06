8e0a3572978395a2db13f271d03595684c959e97
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-06 09:28:06 -0700

    setup

---
+++ src/components/Game.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+// import { rooms, start as currentRoom } from '../rooms';
+// import Player from './Player';
+// import Room from './Room';
+
+export default class Game extends Component {
+
+
+
+
+
+  render() {
+    return (
+      <section>
+        <h2>This is our Game!</h2>
+      </section>
+    )
+  }
+
+}
